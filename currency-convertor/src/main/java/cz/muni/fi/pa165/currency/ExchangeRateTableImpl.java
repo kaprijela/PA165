4855d3dd270cef5f679b39f9ac8f6b8fb987e69b
Gabriela Kandová <gabika.kandova@gmail.com>
2021-03-17 09:56:06 +0100

    Seminar03-Task03: Implemented ExchangeRateTableImpl

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/ExchangeRateTableImpl.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.currency;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class ExchangeRateTableImpl implements ExchangeRateTable {
+    @Override
+    public BigDecimal getExchangeRate(Currency sourceCurrency, Currency targetCurrency) {
+        if (sourceCurrency.getCurrencyCode().equals("EUR") && targetCurrency.getCurrencyCode().equals("CZK")) {
+            return BigDecimal.valueOf(27);
+        }
+        return null;
+    }
+}
