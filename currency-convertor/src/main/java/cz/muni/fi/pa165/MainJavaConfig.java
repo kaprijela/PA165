499fcc72ed71b285b761e5e8f0bde6fb0171cc15
Gabriela Kandová <gabika.kandova@gmail.com>
2021-03-17 10:48:16 +0100

    Seminar03-Task07: Added Java-based configuration for application context

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainJavaConfig.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pa165;
+
+import cz.muni.fi.pa165.currency.CurrencyConvertor;
+import cz.muni.fi.pa165.currency.CurrencyConvertorImpl;
+import cz.muni.fi.pa165.currency.ExchangeRateTable;
+import cz.muni.fi.pa165.currency.ExchangeRateTableImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Configuration
+public class MainJavaConfig {
+    @Bean
+    public ExchangeRateTable getExchangeRateTable() {
+        return new ExchangeRateTableImpl();
+    }
+    @Bean
+    public CurrencyConvertor getCurrencyConvertorImpl() {
+        return new CurrencyConvertorImpl(getExchangeRateTable());
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainJavaConfig.class);
+        CurrencyConvertor cc = applicationContext.getBean(CurrencyConvertorImpl.class);
+
+        System.out.println(cc.convert(
+                Currency.getInstance("EUR"), Currency.getInstance("CZK"), BigDecimal.valueOf(1)
+        ));
+    }
+}
