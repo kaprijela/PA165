4aec2dfdfdadfabd4a248cfb3c99c72c34ef3ada
Gabriela Kandová <gabika.kandova@gmail.com>
2021-03-17 10:02:47 +0100

    Seminar03-Task04: Added XML configuration for application context

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainXml.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165;
+
+import cz.muni.fi.pa165.currency.CurrencyConvertorImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class MainXml {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CurrencyConvertorImpl currencyConvertor = applicationContext.getBean(CurrencyConvertorImpl.class);
+
+        System.out.println(currencyConvertor.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), BigDecimal.valueOf(1)));
+    }
+}
